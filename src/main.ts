288a7efa8c892969b63420403d200cda99040099
gabrielcoffee <cafegabriel1@gmail.com>
2025-07-14 21:03:48 -0400

    port fixed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
